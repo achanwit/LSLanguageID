d12347cca492794b97cf2372dec1ba48965a4f87
achanwit <chanwit-a@hotmail.com>
2021-06-04 15:17:45 +0700

    First Commit

---
+++ src/main/java/com/omniscien/lslanguageid/model/LanguageIDPrepertiesModel.java
@@ -0,0 +1,92 @@
+package com.omniscien.lslanguageid.model;
+
+public class LanguageIDPrepertiesModel {
+	private String CONFIG_PATH_CLEANBADCHARS;
+	private String CLEAN_BAD_CHAR_CONFIG;
+	private String CONFIG_NUM_PUNC;
+	private String RESULT_PATH;
+	private String TEMP_PATH;
+	private String DEBUGPATH;
+	private String PROCESS_TEMP_PATH;
+	private String LOG_PATH;
+	private String LOG_4J;
+
+	public LanguageIDPrepertiesModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCONFIG_PATH_CLEANBADCHARS() {
+		return CONFIG_PATH_CLEANBADCHARS;
+	}
+
+	public void setCONFIG_PATH_CLEANBADCHARS(String cONFIG_PATH_CLEANBADCHARS) {
+		CONFIG_PATH_CLEANBADCHARS = cONFIG_PATH_CLEANBADCHARS;
+	}
+
+	public String getCLEAN_BAD_CHAR_CONFIG() {
+		return CLEAN_BAD_CHAR_CONFIG;
+	}
+
+	public void setCLEAN_BAD_CHAR_CONFIG(String cLEAN_BAD_CHAR_CONFIG) {
+		CLEAN_BAD_CHAR_CONFIG = cLEAN_BAD_CHAR_CONFIG;
+	}
+
+	public String getCONFIG_NUM_PUNC() {
+		return CONFIG_NUM_PUNC;
+	}
+
+	public void setCONFIG_NUM_PUNC(String cONFIG_NUM_PUNC) {
+		CONFIG_NUM_PUNC = cONFIG_NUM_PUNC;
+	}
+
+	public String getRESULT_PATH() {
+		return RESULT_PATH;
+	}
+
+	public void setRESULT_PATH(String rESULT_PATH) {
+		RESULT_PATH = rESULT_PATH;
+	}
+
+	public String getTEMP_PATH() {
+		return TEMP_PATH;
+	}
+
+	public void setTEMP_PATH(String tEMP_PATH) {
+		TEMP_PATH = tEMP_PATH;
+	}
+
+	public String getDEBUGPATH() {
+		return DEBUGPATH;
+	}
+
+	public void setDEBUGPATH(String dEBUGPATH) {
+		DEBUGPATH = dEBUGPATH;
+	}
+
+	public String getPROCESS_TEMP_PATH() {
+		return PROCESS_TEMP_PATH;
+	}
+
+	public void setPROCESS_TEMP_PATH(String pROCESS_TEMP_PATH) {
+		PROCESS_TEMP_PATH = pROCESS_TEMP_PATH;
+	}
+
+	public String getLOG_PATH() {
+		return LOG_PATH;
+	}
+
+	public void setLOG_PATH(String lOG_PATH) {
+		LOG_PATH = lOG_PATH;
+	}
+
+	public String getLOG_4J() {
+		return LOG_4J;
+	}
+
+	public void setLOG_4J(String lOG_4J) {
+		LOG_4J = lOG_4J;
+	}
+	
+	
+
+}
